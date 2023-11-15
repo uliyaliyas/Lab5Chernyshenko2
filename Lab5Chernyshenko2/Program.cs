68c5a8992767168d91bf3e77bfa8afc93e3c0e24
uliyalyaaas <uliyaliya.1304@gmail.com>
2023-11-15 21:26:19 +0200

    Добавьте файлы проекта.

---
+++ Lab5Chernyshenko2/Program.cs
@@ -0,0 +1,11 @@
+﻿string a=Console.ReadLine();
+string[] b = a.Split(' ');
+string[] mas = new string[10];
+for (int i = 0; i < 10; i++)
+{
+    mas[i] = b[i];
+}
+for (int i = 0;i < 10;i++)
+{
+    Console.WriteLine(mas[i]);
+}
